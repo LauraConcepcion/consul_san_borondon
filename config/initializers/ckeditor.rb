28024eefe041058bc3aebb62c8c62510ecd2bc1a
Laura Concepción Rodríguez <usabi@MacBook-Pro-de-Laura.local>
2017-01-16 18:14:33 +0000

    Fixes ckeditor assets precompile problems

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,4 @@
 Ckeditor.setup do |config|
-  #config.assets_languages = I18n.available_locales.map(&:to_s)
-  # config.assets_languages = ['en','es']
-  # config.assets_plugins = []
-  config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
+  config.assets_languages = Rails.application.config.i18n.available_locales.map{|l| l.to_s.downcase}
+  config.assets_plugins = []
 end
